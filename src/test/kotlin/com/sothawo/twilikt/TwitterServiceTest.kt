efd302b3c5dc21d2450aa84a7a674f6084f1f14b
P.J.Meisch <pj.meisch@sothawo.com>
2018-05-21 21:40:22 +0200

    add tests

--- src/test/kotlin/com/sothawo/twilikt/TwitterServiceTest.kt
+++ src/test/kotlin/com/sothawo/twilikt/TwitterServiceTest.kt
@@ -7,14 +7,21 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.*
+import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations.initMocks
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import twitter4j.PagableResponseList
 import twitter4j.Twitter
 import twitter4j.TwitterException
+import java.util.*
+import java.util.stream.Stream
 
 /**
  * @author P.J. Meisch (pj.meisch@sothawo.com)
@@ -45,7 +52,7 @@ internal class TwitterServiceTest {
             // when is a kotlin keyword
             `when`(twitter4j.id).thenReturn(user.id)
             // set up a mock user to be returned from twitter4j
-            val twitterUser = Mockito.mock(twitter4j.User::class.java)
+            val twitterUser = mock(twitter4j.User::class.java)
             `when`(twitterUser.id).thenReturn(user.id)
             `when`(twitterUser.name).thenReturn(user.name)
             `when`(twitterUser.screenName).thenReturn(user.screenName)
@@ -71,6 +78,39 @@ internal class TwitterServiceTest {
 
             assertThatThrownBy({ twitterService.userWithId(42) })
                     .isInstanceOf(TwitterException::class.java)
+        }
+
+        @Mock
+        lateinit var responseList: PagableResponseList<twitter4j.User>
+
+        @Test
+        fun `retrieves the friend's list`() {
+            val expectedFriends = listOf(
+                    User(1, "friend1", "Friend 1", "http://image.ugly/friend1"),
+                    User(2, "friend2", "Friend 2", "http://image.ugly/friend2")
+            )
+
+            // set up mock users to be returned from twitter4j
+            val twitterUser1 = mock(twitter4j.User::class.java)
+            `when`(twitterUser1.id).thenReturn(expectedFriends[0].id)
+            `when`(twitterUser1.name).thenReturn(expectedFriends[0].name)
+            `when`(twitterUser1.screenName).thenReturn(expectedFriends[0].screenName)
+            `when`(twitterUser1.profileImageURLHttps).thenReturn(expectedFriends[0].profileImageUrl)
+            val twitterUser2 = mock(twitter4j.User::class.java)
+            `when`(twitterUser2.id).thenReturn(expectedFriends[1].id)
+            `when`(twitterUser2.name).thenReturn(expectedFriends[1].name)
+            `when`(twitterUser2.screenName).thenReturn(expectedFriends[1].screenName)
+            `when`(twitterUser2.profileImageURLHttps).thenReturn(expectedFriends[1].profileImageUrl)
+
+
+            `when`(responseList.hasNext()).thenReturn(false)
+            `when`(responseList.stream()).thenReturn(Arrays.asList(twitterUser1, twitterUser2).stream())
+
+            `when`(twitter4j.getFriendsList(eq(user.id), anyLong(), anyInt())).thenReturn(responseList)
+
+            assertThat(twitterService.loadFriends(user)).containsExactlyInAnyOrderElementsOf(expectedFriends)
+
+            verify(twitter4j, atLeastOnce()).getFriendsList(eq(user.id), anyLong(), anyInt())
         }
     }
 }
